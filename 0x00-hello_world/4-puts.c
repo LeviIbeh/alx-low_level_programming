8139ff7856bf26df947824ef144f7298c0488ac7
LeviIbeh <levidamilare@gmail.com>
2023-07-13 16:04:40 -0700

    4done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Program iput
+ * main - Program to output zero as its return value
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle")
+return (0)
 }
