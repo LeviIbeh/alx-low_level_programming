dc67b2de8162d42feb82cc1786e48aeaeb2d1580
LeviIbeh <levidamilare@gmail.com>
2023-08-01 17:51:25 -0700

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,14 @@
  * @s: array to set
  * @b: value to set it as
  * @n: n amount of rimes
- * REturn: char value of s
+ * Return: char value of s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	or (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
