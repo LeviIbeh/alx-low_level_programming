91010a9ef7386b606d617575aac3f2740da67d77
LeviIbeh <levidamilare@gmail.com>
2023-08-28 18:29:40 -0700

    kkk

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		tmp = *head;
+		temp = *head;
 		free(*head);
-		*head = tmp->next;
+		*head = temp->next;
 	}
 	*head = NULL;
 }
