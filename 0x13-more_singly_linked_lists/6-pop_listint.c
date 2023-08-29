3137bed79f22c8e9afe3d487f35f83c4a217e633
LeviIbeh <levidamilare@gmail.com>
2023-08-28 18:37:02 -0700

    kkk

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,19 +2,20 @@
 /**
  * pop_listint - pops a node out
  * @head: linked list address
- * Return: head node data
+ * Return: head node data or 0 if empty
  */
 int pop_listint(listint_t **head)
 {
-	int value = 0;
-	listint_t *tmp;
+	listint_t *temp;
+	int num;
 
-	if (*head == NULL)
-		return (value);
-	tmp = *head;
-	value = tmp->n;
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
 	free(*head);
-	*head = tmp->next;
+	*head = temp;
 
-	return (value);
+	return (num);
 }
