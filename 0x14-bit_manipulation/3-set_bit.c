70c04d92a8c1cf8b39378849f726ab16c16c834f
LeviIbeh <levidamilare@gmail.com>
2023-09-28 20:56:17 -0700

    Scared

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * set_bit - sets a bit
- * @n: integer to grab
- * @index: index of bit set
- * Return: 1 for suc, -1 for error
+ * set_bit - sets bit value
+ * @n: number set
+ * @index: location index to set
+ *
+ * Return: 1 in suc, or -1 for error
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	unsigned long int setbit;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-	return (-1);
+	setbit = 1 << index;
+	*n = *n | setbit;
+	return (1);
 }
