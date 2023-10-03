19ccdbda4ef43d2f3081036978436c6b63b64a53
LeviIbeh <levidamilare@gmail.com>
2023-10-03 15:32:52 -0700

    pushed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,18 +8,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t fn;
+	ssize_t writer;
+	ssize_t display;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fn = open(filename, O_RDONLY);
+	if (fn == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	buff = malloc(sizeof(char) * letters);
+	display = read(fn, buff, letters);
+	writer = write(STDOUT_FILENO, buff, display);
 
-	free(buf);
-	close(fd);
-	return (w);
+	free(buff);
+	close(fn);
+	return (writer);
 }
