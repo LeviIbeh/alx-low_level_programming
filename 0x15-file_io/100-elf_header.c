3ee20d1623104985066d0315479555fd9bcd2d5e
LeviIbeh <levidamilare@gmail.com>
2023-10-03 17:40:19 -0700

    end

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,8 @@
+/**
+ * Creator: ONLINE VERSION
+ * beginning: LeviIbeh
+ */
+
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -18,7 +23,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
+ * check_elf - Checks file type
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: If the file is not an ELF file - exit code 98.
