110782fdffb281f6529428f83d0550fb2823d077
LeviIbeh <levidamilare@gmail.com>
2023-10-30 16:47:39 -0700

    okay

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile - reads and prints a textfile
+ * @filename: read file
+ * @letters: bytes of letters read
+ * Return: exact bytes processed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fn;
+	ssize_t writer;
+	ssize_t display;
+
+	fn = open(filename, O_RDONLY);
+	if (fn == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	display = read(fn, buff, letters);
+	writer = write(STDOUT_FILENO, buff, display);
+
+	free(buff);
+	close(fn);
+	return (writer);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+this is the beginning of c19 after deferrment
+0x15-file_io
